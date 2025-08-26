6cc9d4987ac5ba5bfff16e72585a666d7492a957
kangkisun <kangki@hamail.net>
2025-08-26 11:05:53 +0900

    파이선 커밋

---
+++ test.py
@@ -0,0 +1,20 @@
+try:
+
+    3 / 0
+
+except IndexError:
+
+    print("인덱스가 안맞아요!")
+
+
+except ZeroDivisionError:
+
+    print("0으로 나누면 안돼요!")
+
+finally:
+
+    print("예외에 상관없이 언제나 실행되요!")  
+
+ 
+
+print("프로그램이 정상적으로 종료됩니다!")
